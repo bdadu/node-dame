8635f96450a4d238273b6ac4583391b48f9ab54a
Berendi Paul <berendi_paul@yahoo.com>
2019-06-24 21:47:52 +0300

    alpha 1.2

--- public/javascript/dame2.js
+++ public/javascript/dame2.js
@@ -7,25 +7,30 @@ fetch(API.GET)
     return resp.json();
   })
   .then(function (data) {
-    console.log(data);
-    $(document).ready(function () {
-      for (var i = 1; i <= 8; i++) {
-        var divRow = $("<div>", {
-          class: "row",
-        });
-        for (var j = 1; j <= 8; j++) {
-          var div = $("<div>", {
-            class: "square"
-          });
-
-          if (i % 2 == j % 2) {
-            $(div).addClass("white");
-          } else {
-            $(div).addClass("black");
-          }
-          divRow.append(div);
-        }
-        $("#board").append(divRow);
-      }
-    });
+    console.warn('asaza piesele', data);
+    var bile=document.querySelector('.p1-1');
+    bile.classList.add("piece");
   });
+
+
+  const paintEmptyTable = () => {
+    for (var i = 1; i <= 8; i++) {
+      var divRow = $("<div>", {
+        class: "row",
+      });
+      for (var j = 1; j <= 8; j++) {
+        var div = $(`<div class="square p${i}-${j}">`);
+
+        if (i % 2 == j % 2) {
+          $(div).addClass("white");
+        } else {
+          $(div).addClass("black");
+        }
+        divRow.append(div);
+      }
+      $("#board").append(divRow);
+    }
+    
+  }
+
+  paintEmptyTable();
