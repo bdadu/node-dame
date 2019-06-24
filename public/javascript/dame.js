77ef2540a9391a5c8e9008ecb76971c59fcd39f1
Berendi Paul <berendi_paul@yahoo.com>
2019-06-24 20:07:35 +0300

    beta 1.1

--- public/javascript/dame.js
+++ public/javascript/dame.js
@@ -1,12 +1,11 @@
 let board = document.getElementsByClassName("dame")[0];
 let pieceCode = '\u2B24';
 
-
 Array.from(board.children).forEach(function(cell) {
   cell.onclick = function(elem) { 
     if (elem.target.innerHTML === pieceCode) {
         from = elem.target;
-    } else if (from && diagonalMove(from, elem.target)) {
+   } else if (from && diagonalMove(from, elem.target)) {
         elem.target.innerHTML = pieceCode;
         from.innerHTML = '';
         from = null;
---
+++ public/javascript/dame2.js
@@ -0,0 +1,31 @@
+var API = {
+  GET: './data/board-short.json'
+};
+
+fetch(API.GET)
+  .then(function (resp) {
+    return resp.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    $(document).ready(function () {
+      for (var i = 1; i <= 8; i++) {
+        var divRow = $("<div>", {
+          class: "row",
+        });
+        for (var j = 1; j <= 8; j++) {
+          var div = $("<div>", {
+            class: "square"
+          });
+
+          if (i % 2 == j % 2) {
+            $(div).addClass("white");
+          } else {
+            $(div).addClass("black");
+          }
+          divRow.append(div);
+        }
+        $("#board").append(divRow);
+      }
+    });
+  });
