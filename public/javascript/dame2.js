f10d69d5ba5632ae80e20dca731063ec7353eaaa
Bogdan Dadu <dadu.bogdan@gmail.com>
2019-07-01 21:11:37 +0300

    putPiece1.1

--- public/javascript/dame2.js
+++ public/javascript/dame2.js
@@ -1,36 +1,49 @@
+var myPieces=[];
 var API = {
   GET: './data/board-short.json'
 };
 
 fetch(API.GET)
-  .then(function (resp) {
-    return resp.json();
-  })
-  .then(function (data) {
-    console.warn('asaza piesele', data);
-    var bile=document.querySelector('.p1-1');
-    bile.classList.add("piece");
-  });
+ .then(function (resp) {
+   return resp.json();
+ })
+ .then(function (piece) {
 
+   myPiece=piece;
+   Dame.display(piece);
+ });
 
-  const paintEmptyTable = () => {
-    for (var i = 1; i <= 8; i++) {
-      var divRow = $("<div>", {
-        class: "row",
-      });
-      for (var j = 1; j <= 8; j++) {
-        var div = $(`<div class="square p${i}-${j}">`);
+ const Dame= {
+   display:function (pieces){
+     for( var i=1; i<)
+    document.querySelector(".pi-3").classList.add("piece2")
+    document.querySelector(".p2-3").classList.add("piece1")
+   }
+ }
+   
 
-        if (i % 2 == j % 2) {
-          $(div).addClass("white");
-        } else {
-          $(div).addClass("black");
-        }
-        divRow.append(div);
+paintEmptyTable = () => {
+  for (var i = 1; i <= 8; i++) {
+    var divRow = $("<div>", {
+      class: "row",
+    });
+    for (var j = 1; j <= 8; j++) {
+      var div = $(`<div class="square p${i}-${j}">`);
+
+      if (i % 2 == j % 2) {
+        $(div).addClass("white");
+      } else {
+        $(div).addClass("black");
       }
-      $("#board").append(divRow);
+      divRow.append(div);
     }
-    
+    $("#board").append(divRow);
   }
 
-  paintEmptyTable();
+}
+paintEmptyTable();
+
+//Raul's cod for piece selection and diagonale move...to be update
+
+
+
