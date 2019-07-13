72334fec99edbbbdb727630044cc3acfdcdeca22
Berendi Paul <berendi_paul@yahoo.com>
2019-07-13 12:53:52 +0300

    added sql export

--- routes/users-db.js
+++ routes/users-db.js
@@ -13,11 +13,7 @@ var pool  = mysql.createPool({
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   pool.getConnection((err, connection) => {
-<<<<<<< HEAD
     const sql = `SELECT * FROM  pices`;
-=======
-    const sql = `SELECT * FROM  pozitii piese`;
->>>>>>> 2a7c24a447505b980d87ea53965878a0d5d4cb08
     connection.query(sql, (err, results) => {
       res.json(results);
     });
