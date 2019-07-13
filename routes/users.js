4db97d99c9a062b9287a11131500d467c28c66fb
Berendi Paul <berendi_paul@yahoo.com>
2019-07-13 12:46:29 +0300

    update

--- routes/users.js
+++ routes/users.js
@@ -7,3 +7,31 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+router.post('/add', function (req, res, next) {
+  var x = req.body.x;
+  var y = req.body.y;
+  var piece = req.body.piece;
+  console.warn('add', x, y, piece);
+  var piece = require('../public/data/board-short.json');
+
+   const id=new Date().getTime()
+  myPieces.push({
+    x,
+    y,
+    piece
+    
+  });
+
+
+
+
+  var str = JSON.stringify(persons, null, 2);
+  fs.writeFileSync('./public/data/persons.json', str);// se sterge in fiser ce a fost si se salveaza noile date
+
+
+  res.json({
+    success: true,
+    id,
+    message: 'DONE'
+  });
+});
