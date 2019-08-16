a4533d817f047a56d81d2daa4b1f6a656251c10b
Bogdan Dadu <dadu.bogdan@gmail.com>
2019-08-16 10:57:40 +0300

    king rules

--- public/javascript/dame2.js
+++ public/javascript/dame2.js
@@ -131,7 +131,7 @@ function tryToMove(piece, x, y) {
 
     }
     // piesa obisnuita devine king (UK ) sau Dama (RS)
-    if (piece.x == 8) {
+    if (piece.piece == 1 && piece.x == 8) {
       piece.piece = 3;
 
     }
